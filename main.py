3cc4e221ccaee4d48029c51f1f242d4a18a3f079
Tina Thomas <tinamariathomas@gmail.com>
2025-09-16 17:21:08 -0500

    Classify user query

--- main.py
+++ main.py
@@ -18,13 +18,50 @@ def get_completion_from_messages(messages,
     )
     return response.choices[0].message["content"]
 
+delimiter = "####"
+system_message = f"""
+You will be provided with customer service queries. \
+The customer service query will be delimited with \
+{delimiter} characters.
+Classify each query into a primary category \
+and a secondary category. 
+Provide your output in json format with the \
+keys: primary and secondary.
+
+Primary categories: Billing, Technical Support, \
+Account Management, or General Inquiry.
+
+Billing secondary categories:
+Unsubscribe or upgrade
+Add a payment method
+Explanation for charge
+Dispute a charge
+
+Technical Support secondary categories:
+General troubleshooting
+Device compatibility
+Software updates
+
+Account Management secondary categories:
+Password reset
+Update personal information
+Close account
+Account security
+
+General Inquiry secondary categories:
+Product information
+Pricing
+Feedback
+Speak to a human
+
+"""
+user_message = f"""\
+I want you to delete my profile and all of my user data"""
 messages =  [  
 {'role':'system', 
- 'content':"""You are an assistant who\
- responds in the style of Dr Seuss."""},    
+ 'content': system_message},    
 {'role':'user', 
- 'content':"""write me a very short poem\
- about a happy carrot"""},  
+ 'content': f"{delimiter}{user_message}{delimiter}"},  
 ] 
-response = get_completion_from_messages(messages, temperature=1)
+response = get_completion_from_messages(messages)
 print(response)
