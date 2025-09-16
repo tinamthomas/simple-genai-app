bffa2fdc9cc6bf9a6f6fdbe5e6389343325a5bae
Tina Thomas <tinamariathomas@gmail.com>
2025-09-16 16:43:09 -0500

    Updating get_completion to use messages

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
+def get_completion_from_messages(messages, 
+                                 model="gpt-3.5-turbo", 
+                                 temperature=0, 
+                                 max_tokens=500):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output 
+        temperature=temperature, # this is the degree of randomness of the model's output
+        max_tokens=max_tokens, # the maximum number of tokens the model can ouptut 
     )
     return response.choices[0].message["content"]
 
-response = get_completion("What is the capital of France?")
+messages =  [  
+{'role':'system', 
+ 'content':"""You are an assistant who\
+ responds in the style of Dr Seuss."""},    
+{'role':'user', 
+ 'content':"""write me a very short poem\
+ about a happy carrot"""},  
+] 
+response = get_completion_from_messages(messages, temperature=1)
 print(response)
